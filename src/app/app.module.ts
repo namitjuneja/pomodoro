28c8938ee70b84ede63f364e94cb45ebf9eecf18
namitjuneja <juneja.namit@gmail.com>
2020-03-04 19:56:39 -0500

    created settings and pomodoro-timer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings/settings.component';
+import { PomodoroTimerComponent } from './pomodoro-timer/pomodoro-timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    PomodoroTimerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pomodoro-timer/pomodoro-timer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pomodoro-timer',
+  templateUrl: './pomodoro-timer.component.html',
+  styleUrls: ['./pomodoro-timer.component.css']
+})
+export class PomodoroTimerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
